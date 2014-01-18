8daadf7bbfcaae9ad92b8912f71dbcbb5c11936d
Justin <n56vz>
2014-01-18 04:42:20 -0500

    Add (easy) timestamp function to genericUtils.py

--- genericUtils.py
+++ genericUtils.py
@@ -1,4 +1,49 @@
-import re, os, sys
+import re, os, sys, time
+
+
+def timestamp(d=True, t=True, tz=True, utc=False, winsafe=True):
+    '''Simple utility to print out a time, date, or time&date stamp,
+    with some reconfigurability for commonly-used options. Default is in
+    ISO8601 format without colons separating hours, min, and sec to avoid
+    file naming issues.
+
+    Options:
+        d          print date (default: True)
+        t          print time (default: True)
+        tz         print timezone offset from UTC (default: True)
+        utc        print time/date in UTC (default: False)
+        winsafe    omit colons between hours/minutes (default: True)
+
+    '''
+    if utc:
+        timeTuple = time.gmtime()
+    else:
+        timeTuple = time.localtime()
+
+    dts = ""
+    if d:
+        dts += time.strftime("%Y-%m-%d", timeTuple)
+        if t:
+            dts += "T"
+    if t:
+        if winsafe:
+            dts += time.strftime("%H%M%S", timeTuple)
+        else:
+            dts += time.strftime("%H:%M:%S", timeTuple)
+
+        if tz:
+            if utc:
+                if winsafe:
+                    dts += time.strftime("+0000")
+                else:
+                    dts += time.strftime("+00:00")
+            else:
+                offset = time.strftime("%z")
+                if not winsafe:
+                    offset = offset[:-2:] + ":" + offset[-2::]
+                dts += offset
+    return dts
+
 
 def findFiles(directory, regex):
     if isinstance(regex, (str, unicode)):
@@ -9,12 +54,12 @@ def findFiles(directory, regex):
                 filename = os.path.join(root, basename)
                 yield (root, basename) #filename
 
+
 def wstdout(x):
     sys.stdout.write(x)
     sys.stdout.flush()
 
+
 def wstderr(x):
     sys.stderr.write(x)
     sys.stderr.flush()
-
-
